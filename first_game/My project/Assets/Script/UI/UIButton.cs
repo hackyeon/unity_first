b423a4162d30a9e2700977233d4d51a901e8a47c
hackyeon <skek933@gmail.com>
2022-01-30 11:26:05 +0900

    commit

--- first_game/My project/Assets/Script/UI/UIButton.cs
+++ first_game/My project/Assets/Script/UI/UIButton.cs
@@ -1,15 +1,47 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Object = UnityEngine.Object;
 
 public class UIButton : MonoBehaviour
 {
-    [SerializeField] private Text mText;
+    private Dictionary<Type, UnityEngine.Object[]> mObject = new Dictionary<Type, Object[]>();
+
+    enum Buttons
+    {
+        PointButton
+    }
+    enum Texts
+    {
+        PointText,
+        ScoreText
+    }
+
+    void Bind<T>(Type type) where T : UnityEngine.Object
+    {
+        string[] names = Enum.GetNames(type);
+        UnityEngine.Object[] objects = new UnityEngine.Object[names.Length];
+        mObject.Add(typeof(T), objects);
+
+        for (int i = 0; i < names.Length; i++)
+        {
+            objects[i] = Util.FindChild<T>(gameObject, names[i], true);
+        }
+    }
+
+    private void Start()
+    {
+        Bind<Button>(typeof(Buttons));
+        Bind<Text>(typeof(Types));
+    }
+
+
     private int mScore = 0;
     public void OnButtonClicked()
     {
         mScore++;
-        mText.text = $"점수: {mScore}점";
+        // mText.text = $"점수: {mScore}점";
     }
 }
